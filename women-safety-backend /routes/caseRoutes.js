dea13aabe3f310e72b3c0363a22ebb408ebdce2b
MOHANAPRIYA V <mv2722@srmist.edu.in>
2025-05-05 19:09:37 +0530

    Delete women-safety-backend /routes/caseRoutes.js

--- women-safety-backend /routes/caseRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { assignCase, updateCaseStatus, getCasesByPolice } = require('../controllers/caseController');
-
-router.post('/assign', assignCase);
-router.put('/update/:caseId', updateCaseStatus);
-router.get('/police/:policeId', getCasesByPolice);
-
-module.exports = router;
