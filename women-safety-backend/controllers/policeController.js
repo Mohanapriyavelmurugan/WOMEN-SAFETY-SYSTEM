5f71f87249346e11805c772fa6fdd159cab556e1
MOHANAPRIYA V <mv2722@srmist.edu.in>
2025-05-05 18:30:23 +0530

    Update policeController.js

--- women-safety-backend/controllers/policeController.js
+++ women-safety-backend/controllers/policeController.js
@@ -1,1 +1,21 @@
+const db = require('../db');
+
+const addPolice = (req, res) => {
+  const { name, badge_number, contact } = req.body;
+  const sql = 'INSERT INTO police (name, badge_number, contact) VALUES (?, ?, ?)';
+  db.query(sql, [name, badge_number, contact], (err, result) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.status(201).json({ message: 'Police officer added' });
+  });
+};
+
+const getAllPolice = (req, res) => {
+  const sql = 'SELECT * FROM police';
+  db.query(sql, (err, results) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.status(200).json(results);
+  });
+};
+
+module.exports = { addPolice, getAllPolice };
 
